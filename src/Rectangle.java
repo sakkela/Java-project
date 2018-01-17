1927f97cac1e3cb3cbb80ca4640666870d8c888d
Tosin Ayanrinola <sakkela@hotmail.com>
2018-01-17 14:18:14 +0000

    Automated Java build

---
+++ src/Rectangle.java
@@ -0,0 +1,17 @@
+public class Rectangle {
+  public int length;
+  public int width;
+  
+  public Rentangle(int length, int width){
+    this.length = length;
+    this.width = width;
+  }
+  public int getArea(){
+    returns length * width;
+  } 
+
+ public int getPerimeter(){
+   return 2 * (lenght + width);
+ }
+
+}
---
+++ src/Rectangulator.java
@@ -0,0 +1,14 @@
+public class Rectangulator {
+ 
+  public static void main (String [] args){
+    int length = Integer.parseInt(arg[0]);
+    int width = Interger.perseInt(arg[1]):
+   
+    Rectangle myRec = new Rectangle( length, width);
+    
+    String output = String.format("*** Your Rectangle *** \n\nLenght: %d\nWidth: %d\nArea: %d\nPerimeter: %d\n\n", myRec.length, myRec.width, myRec.getArea(), myRec.getPerimetar());
+    
+    System.out.printlin(output);
+  {
+ 
+}
