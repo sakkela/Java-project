98c2fe7ee332a526486403be0e5d4ad6b95a7fc8
Tosin Ayanrinola <sakkela@hotmail.com>
2018-01-18 16:36:26 +0000

    Jenkins buildfile

--- Jenkinsfile
+++ Jenkinsfile
@@ -1,1 +1,10 @@
-Test
+pipeline {
+    agent any
+
+    stages {
+        stage('build'){
+            sh 'ant -f build.xml -v'
+        }
+    }
+
+}
--- src/Rectangle.java
+++ src/Rectangle.java
@@ -2,16 +2,16 @@ public class Rectangle {
   public int length;
   public int width;
   
-  public Rentangle(int length, int width){
+  public Rectangle(int length, int width){
     this.length = length;
-    this.width = width;
+    this.width= width;
   }
   public int getArea(){
-    returns length * width;
+    return length * width;
   } 
 
  public int getPerimeter(){
-   return 2 * (lenght + width);
+   return 2 * (length + width);
  }
 
 }
